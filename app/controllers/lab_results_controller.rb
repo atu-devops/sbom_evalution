32ad7ec2195a6216c155a425a4ae791eebfe0c6a
Owen Connor <owenconnor@protonmail.com>
2023-11-02 17:32:08 +0000

    added basic controller methods and views

--- app/controllers/lab_results_controller.rb
+++ app/controllers/lab_results_controller.rb
@@ -1,2 +1,20 @@
 class LabResultsController < ApplicationController
+  def new
+  end
+
+  def upload
+    uploaded_file = params[:lab_file]
+    @parsed_results = parse_file(uploaded_file.read)
+    @parsed_results = PARSED_RESULTS
+    render :show
+  end
+
+  private
+  def parse_file(file_contents)
+    [  # This is just an example of what the parsed results might look like
+      { code: 'C100', result: 1.0, format: 'float', comment: ["I'm a comment"] },
+      { code: 'C200', result: 2.0, format: 'float', comment: ["I'm a comment too"] }
+    ]
+  end
+
 end
