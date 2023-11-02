87ac2242b03db77f1f7a66855b0ec92c0d541752
Owen Connor <owenconnor@protonmail.com>
2023-11-02 18:45:58 +0000

    moved parsing to service object

--- app/controllers/lab_results_controller.rb
+++ app/controllers/lab_results_controller.rb
@@ -15,60 +15,9 @@ class LabResultsController < ApplicationController
       return render json: { error: 'Unsupported file type' }, status: :unprocessable_entity
     end
 
-    @parsed_results = parse_file(uploaded_file.read)
+    parser = LabFileParser.new(uploaded_file.read)
+    @parsed_results = parser.parse_file
     render :show
-  end
-
-  private
-
-  def parse_file(file_content)
-    lines = file_content.split("\n")
-    results = []
-    current_obx = nil
-
-    lines.each do |line|
-      parts = line.split('|')
-
-      # Parse OBX lines
-      if parts[0] == 'OBX'
-        current_obx = {
-          code: parts[2],
-          result: transform_result(parts[2], parts[3]),
-          format: get_format(parts[2]),
-          comment: []
-        }
-        results << current_obx
-      end
-
-      # Parse NTE lines
-      if parts[0] == 'NTE' && current_obx
-        current_obx[:comment] << parts[2]
-      end
-    end
-
-    results
-  end
-
-  def transform_result(code, value)
-    case get_format(code)
-    when 'float'
-      value.to_f
-    when 'boolean'
-      value == 'NEGATIVE' ? -1.0 : -2.0
-    when 'nil_3plus'
-      { 'NIL' => -1.0, '+' => -2.0, '++' => -2.0, '+++' => -3.0 }[value]
-    else
-      value
-    end
-  end
-
-  def get_format(code)
-    {
-      'C100' => 'float',
-      'C200' => 'float',
-      'A250' => 'boolean',
-      'B250' => 'nil_3plus'
-    }[code]
   end
 
 end
---
+++ app/services/lab_file_parser.rb
@@ -0,0 +1,57 @@
+class LabFileParser
+  attr_reader :file, :results
+
+  def initialize(file)
+    @file = file
+    @results = []
+  end
+
+  def parse_file
+    lines = @file.split("\n")
+    current_obx = nil
+
+    lines.each do |line|
+      parts = line.split('|')
+
+      # Parse OBX lines
+      if parts[0] == 'OBX'
+        current_obx = {
+          code: parts[2],
+          result: transform_result(parts[2], parts[3]),
+          format: get_format(parts[2]),
+          comment: []
+        }
+        @results << current_obx
+      end
+
+      # Parse NTE lines
+      if parts[0] == 'NTE' && current_obx
+        current_obx[:comment] << parts[2]
+      end
+    end
+
+    @results
+  end
+
+  def transform_result(code, value)
+    case get_format(code)
+    when 'float'
+      value.to_f
+    when 'boolean'
+      value == 'NEGATIVE' ? -1.0 : -2.0
+    when 'nil_3plus'
+      { 'NIL' => -1.0, '+' => -2.0, '++' => -2.0, '+++' => -3.0 }[value]
+    else
+      value
+    end
+  end
+
+  def get_format(code)
+    {
+      'C100' => 'float',
+      'C200' => 'float',
+      'A250' => 'boolean',
+      'B250' => 'nil_3plus'
+    }[code]
+  end
+end
